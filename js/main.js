781e2b90615cfe6074ed5b07fee68237af75096e
Gonzalo Souto <gonzalosdesign@gmail.com>
2023-05-26 20:48:35 -0300

    form fetched

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-/*
-the following code is meant to simulate a personal money manager
-it should let the user to set credits and debit through a form
-when th user clicks the button shoots an event
-a function will creat an object with th form inputs and pushed it into an global array
-each time the user send a movemnt the dom print a new div showing the whole array
 
-extra features
-there would be another form for searching movents*/
+
+let movementCreationForm = document.getElementById("movementCreationForm");
+// console.log(movementCreationForm);
+
+movementCreationForm.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    let input = e.target.children;
+    console.log(input[0].value);
+    console.log(input[3].value);
+    console.log(input[6].value);
+    // console.log(input[9].value);
+    
+})
---
+++ js/movementsClass.js
@@ -0,0 +1,7 @@
+class Movements {
+    constructor (movementDescription, movementImport, movementType) {
+            this.movementDescription = movementDescription;
+            this.movementImport = movementImport;
+            this.movementType = movementType;
+    }
+};
